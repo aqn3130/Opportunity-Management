9809e7a881a60897e5f2e19bc0a45b295e36be49
aqn3130 <ahmad.qurbanzada@springernature.com>
2020-08-29 10:55:41 +0100

    dashboard table layout

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -13,22 +13,30 @@
         </v-card>
         <v-layout>
             <v-container class="mb-10 pa-5" style="z-index: 2; margin-top: -150px;">
-                <v-card class="d-flex ml-auto mr-auto justify-center transparent" flat>
+                <v-card class="d-flex ml-auto mr-auto justify-center transparent" flat width="auto">
                     <v-data-table
                             :headers="headers"
                             :items="rows"
                             :items-per-page="5"
-                            class="elevation-1 body-2 px-3"
+                            class="elevation-1 px-3"
                             :footer-props="{
-                      showFirstLastPage: true,
-                      firstIcon: 'mdi-arrow-collapse-left',
-                      lastIcon: 'mdi-arrow-collapse-right',
-                      prevIcon: 'mdi-minus',
-                      nextIcon: 'mdi-plus'
-                    }"
+                              showFirstLastPage: true,
+                              firstIcon: 'mdi-arrow-collapse-left',
+                              lastIcon: 'mdi-arrow-collapse-right',
+                              prevIcon: 'mdi-minus',
+                              nextIcon: 'mdi-plus'
+                            }"
                     >
                         <template v-slot:item.OpportunityName="{ item }">
-                            {{item.OpportunityName}}<br>{{ item.Country }}<br>{{item.Status}}
+                            <span style="font-size: small; font-weight: bold;">
+                                {{item.OpportunityName}}
+                            </span><br>
+                            <span style="font-size: small">
+                            {{ item.SalesStage }}<br>
+                            </span>
+                            <span style="font-size: small; max-width: 200px" class="text-truncate d-inline-block">
+                            {{item.CustomerName}}-{{item.BPID}}
+                            </span>
                         </template>
                     </v-data-table>
                 </v-card>
@@ -48,40 +56,7 @@
         },
         data() {
             return {
-                rows: [
-                    // {
-                    //     name: 'Frozen Yogurt',
-                    //     calories: 159,
-                    //     fat: 6.0,
-                    //     carbs: 24,
-                    //     protein: 4.0,
-                    //     iron: '1%',
-                    // },
-                    // {
-                    //     name: 'Ice cream sandwich',
-                    //     calories: 237,
-                    //     fat: 9.0,
-                    //     carbs: 37,
-                    //     protein: 4.3,
-                    //     iron: '1%',
-                    // },
-                    // {
-                    //     name: 'Eclair',
-                    //     calories: 262,
-                    //     fat: 16.0,
-                    //     carbs: 23,
-                    //     protein: 6.0,
-                    //     iron: '7%',
-                    // },
-                    // {
-                    //     name: 'Cupcake',
-                    //     calories: 305,
-                    //     fat: 3.7,
-                    //     carbs: 67,
-                    //     protein: 4.3,
-                    //     iron: '8%',
-                    // },
-                ],
+                rows: [],
                 headers: [
                     {text: 'Opportunity Name', align: 'left', value: 'OpportunityName'},
                     {text: 'Country', align: 'left', value: 'Country'},
@@ -91,16 +66,6 @@
                     {text: 'Expected Close Date', align: 'left', value: 'ExpectedCloseDate'},
                     {text: 'Currency', align: 'left', value: 'Currency'},
                     {text: 'Gross Value', align: 'left', value: 'GrossValue'},
-                    // {
-                    //     text: 'Dessert (100g serving)',
-                    //     align: 'left',
-                    //     value: 'name',
-                    // },
-                    // { text: 'Calories', align: 'left', value: 'calories' },
-                    // { text: 'Fat (g)', align: 'left', value: 'fat' },
-                    // { text: 'Carbs (g)', align: 'left', value: 'carbs' },
-                    // { text: 'Protein (g)', align: 'left', value: 'protein' },
-                    // { text: 'Iron (%)', align: 'left', value: 'iron' },
                 ]
             }
         },
