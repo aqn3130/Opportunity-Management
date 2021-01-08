ddf4748b4f39f1188aa217e827e33be83ceafc6a
aqn3130 <ahmad.qurbanzada@springernature.com>
2021-01-08 13:19:15 +0000

    sap-controller

--- server/app/Controllers/Http/SapController.js
+++ server/app/Controllers/Http/SapController.js
@@ -30,72 +30,36 @@ class SapController {
   }
   async store ({ request, response }) {
     const { data } = request.all();
-    let dataValid = true;
-    // console.log(data);
+    const currency = ['EUR', 'USD', 'GBP', 'JPY', 'AUD'];
     const dateRule = {
       validDate: [validations.dateFormat(['YYYY-MM-DD HH:mm:ss'])]
     }
     const rules = {
       SalesRep: 'required',
-      Email: 'required|email',
-      // Type: 'required|in:SPS,ISM',
+      Type: 'required|in:SPS,ISM',
       OpportunityName: 'required',
       CustomerName: 'required',
       ChannelType: 'required|in:Academic,Corporate,Government',
-      Origin: 'required|in:Marketing,Sales Originated,Renewal',
-      OpportunityStartDate: [
-        rule('required'),
-        rule('date'),
-        rule('dateFormat', 'YYYY-MM-DD HH:mm:ss')
-      ],
-      Status: 'required|in:In Process,Won,Closed Won,Closed Lost',
-      SalesStage: 'required|in:1 - Prospect / Lead,2 - Pre-Qualification,3 - Qualified,4 - Needs Assessment,5 - Product Evaluation,6 - Proposal Sent,7 - Negotiating Commercial,8 - Negotiating LC/Legal,9 - Final Terms Accepted,10 - License Delivered to Customer',
-      ExpectedCloseDate: [
-        rule('required'),
-        rule('date'),
-        rule('dateFormat', 'YYYY-MM-DD HH:mm:ss')
-      ],
-      Currency: 'required|in:EUR,USD,GBP,JPY,AUD',
+      Currency: 'required'
     }
     for (let i = 0; i < data.length; i += 1) {
       const validation = await validateAll(data[i], rules);
       if(validation.fails()) {
-        // return validation.messages();
         response.status(400).send(validation.messages());
         return;
       }
     }
 
-    async function getSalesRepType(email) {
-      const user = await request.Knex('SalesRep').where('Email', email);
-      let type = null;
-      if (user.length) {
-        type = await request.Knex('SalesRep').where('Email', email).select('Type');
-        if (type.length) type = type[0].Type;
-      } else {
-        let fullName = email.substring(0, email.indexOf('@')).toLowerCase();
-        type = await request.Knex('SalesRep').where('Full_Name', fullName).select('Type');
-        if (type.length) type = type[0].Type;
-      }
-
-      if (type !== 'SPS' && type !== 'ISM') {
-        dataValid = false;
-      }
-      return type;
-    }
-
     try {
       for (let i = 0; i < data.length; i += 1) {
         data[i].CreationDate = new Date().toISOString().substr(0, 18);
+        data[i].OpportunityStartDate = new Date().toISOString().substr(0, 18);
+        data[i].ExpectedCloseDate = new Date(new Date().getFullYear(), 11, 31).toISOString().substr(0, 18);
+        data[i].Origin = 'Marketing';
+        data[i].SalesStage = '1 - Prospect / Lead';
+        data[i].Status = 'In Process';
         data[i].source = 'SAP';
-        const type = await getSalesRepType(data[i].Email);
-        if (type) data[i].Type = type;
-        delete data[i].Email;
-      }
-
-      if (!dataValid) {
-        response.status(400).send('Sales Rep not found');
-        return;
+        if (!currency.includes(data[i].Currency)) delete data[i].Currency;
       }
 
       const created_ids = [];
@@ -115,7 +79,7 @@ class SapController {
       response.status(201).send(created_opp);
     } catch (e) {
       console.log(e);
-      response.send(e);
+      response.status(400).send(e.sqlMessage);
     }
   }
 }
